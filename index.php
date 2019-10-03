ff122ae8f3292c3ba06b2e38d9700ee894715b65
Voltext <lokin84250@live.fr>
2019-10-03 15:42:57 +0200

    Serialize to UIKIT

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php include 'include/header.php'; ?>
+<div class="ui segment">
+  <div class="ui left rail">
+    <div class="ui segment">
+      Left Rail Content
+    </div>
+  </div>
+  <div class="ui right rail">
+    <div class="ui segment">
+      Right Rail Content
+    </div>
+  </div>
+  <p></p>
+  <p></p>
+</div>
