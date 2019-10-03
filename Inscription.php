c19ecf7f31bfd3ef7ebe8b7e46e01df394648fcf
soufiane <soufiane_dri130@hotmail.fr>
2019-10-03 15:40:08 +0200

    Fin inscription

--- Inscription.php
+++ Inscription.php
@@ -1,40 +1,157 @@
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+<!-- DEBUT INSCRIPTION -->
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <!-- Site Properties -->
+  <title>Login Example - Semantic</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+
+  <script src="assets/library/jquery.min.js"></script>
+  <script src="../dist/components/form.js"></script>
+  <script src="../dist/components/transition.js"></script>
+
+  <style type="text/css">
+    body {
+      background-color: #DADADA;
+    }
+    body > .grid {
+      height: 100%;
+    }
+    .image {
+      margin-top: -100px;
+    }
+    .column {
+      max-width: 450px;
+    }
+  </style>
+  <script>
+  $(document)
+    .ready(function() {
+      $('.ui.form')
+        .form({
+          fields: {
+            email: {
+              identifier  : 'email',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your e-mail'
+                },
+                {
+                  type   : 'email',
+                  prompt : 'Please enter a valid e-mail'
+                }
+              ]
+            },
+            password: {
+              identifier  : 'password',
+              rules: [
+                {
+                  type   : 'empty',
+                  prompt : 'Please enter your password'
+                },
+                {
+                  type   : 'length[6]',
+                  prompt : 'Your password must be at least 6 characters'
+                }
+              ]
+            }
+          }
+        })
+      ;
+    })
+  ;
+  </script>
+</head>
+<body>
 
 <div class="ui middle aligned center aligned grid">
   <div class="column">
     <h2 class="ui teal image header">
-      <img src="assets/images/logo.png" class="image">
+      <img src="https://semantic-ui.com/examples/assets/images/logo.png" class="image">
       <div class="content">
-        Log-in to your account
-      </div>d
+        Inscription
+      </div>
     </h2>
     <form class="ui large form">
       <div class="ui stacked segment">
         <div class="field">
           <div class="ui left icon input">
             <i class="user icon"></i>
-            <input type="text" name="email" placeholder="E-mail address">
-          </div>
+            <input type="text" name="NomFamille" placeholder="Nom de Famille">
+          </div>        
         </div>
+
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input type="text" name="Prenom" placeholder="Prénom">
+          </div>        
+        </div>
+
+         <div class="field">
+          <div class="ui left icon input">
+            <i class="user icon"></i>
+            <input type="text" name="Pseudo" placeholder="Pseudo">
+          </div>        
+        </div>
+
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="mail icon"></i>
+            <input type="text" name="email" placeholder="Adresse mail">
+          </div>        
+        </div>
+
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="calendar icon"></i>
+            <input type="date" name="Dtnaissance" placeholder="JJ/MM/AAAA">
+          </div>        
+        </div>
+
         <div class="field">
           <div class="ui left icon input">
             <i class="lock icon"></i>
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Mot de passe">
           </div>
         </div>
-        <div class="ui fluid large teal submit button">Login</div>
+
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="lock icon"></i>
+            <input type="password" name="confirmpassword" placeholder="Confirmer le mot de passe">
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="ui left icon input">
+            <i class="folder icon"></i>
+            <input type="file" name="fichieravatar" placeholder="Choisir avatar">
+          </div>
+        </div>
+
+        <div class="ui fluid large teal submit button">S'inscrire</div>
       </div>
 
       <div class="ui error message"></div>
 
     </form>
 
-    <div class="ui message">
-      New to us? <a href="#">Sign Up</a>
+    
+      <div class="ui fluid large teal submit button"><a href="#" style="color: white">Connexion</a></div>
     </div>
-  </div>
+  
 </div>
-
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.js"/>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"/>
+</body>
+
+</html>
+<!-- FIN INSCRIPTION -->
