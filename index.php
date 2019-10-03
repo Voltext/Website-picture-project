5e12a0726156bcfa6f2ed62011e816893f061c40
BENMEZIANE <hugo.benmeziane89@gmail.com>
2019-10-03 15:46:10 +0200

    ajout index

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php include 'include/header.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PhotoSpot</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.26.4/css/uikit.min.css" />
+            <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.26.4/css/uikit.min.css"></script>
+            
+            <nav class="uk-navbar">
+              <a href="" class="uk-navbar-toggle"></a>
+              <a href="#" class="uk-navbar-brand">PhotoSpot</a>
+              <ul class="uk-navbar-nav">
+                <li><a href="">Inscription</a></li>
+                <li><a href="">Connexion</a></li>
+              </ul>
+            </nav>
+    </body>
+    <footer>
+    </footer>
+</html>
