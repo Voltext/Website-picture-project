5c5924bd9d29aad4051b0c9d15e3514405a186db
Voltext <lokin84250@live.fr>
2019-10-03 16:24:46 +0200

    Header

--- include/header.php
+++ include/header.php
@@ -10,4 +10,48 @@
         <script src="js/uikit.min.js"></script>
         <script src="js/uikit-icons.min.js"></script>
     </head>
-    
+    </body>
+    <nav class="uk-navbar-container uk-margin" uk-navbar="mode: click">
+
+    <div class="nav-overlay uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" href="#">PhotoSpot</a>
+
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="#">Accueil</a></li>
+            <li><a href="#">Photos</a></li>
+        </ul>
+
+    </div>
+
+    <div class="nav-overlay uk-navbar-right" style="margin-right: 20px;">
+
+        <a class="uk-navbar-toggle" uk-search-icon uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+        <ul class="uk-navbar-nav">
+            <li>
+                <span uk-icon="user" style="cursor: pointer;"></span>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div class="nav-overlay uk-navbar-left uk-flex-1" hidden>
+
+        <div class="uk-navbar-item uk-width-expand">
+            <form class="uk-search uk-search-navbar uk-width-1-1">
+                <input class="uk-search-input" type="search" placeholder="Recherchez un mot clé ou le titre d'une photo" style="text-align: center; " autofocus>
+            </form>
+        </div>
+
+        <a class="uk-navbar-toggle" uk-close uk-toggle="target: .nav-overlay; animation: uk-animation-fade" href="#"></a>
+
+    </div>
+
+</nav>
