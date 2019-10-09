523726c40bbf5b8cca0fdde0918447506746b71b
soufiane <soufiane_dri130@hotmail.fr>
2019-10-09 13:50:54 +0200

    organisation code

--- Inscription.php
+++ Inscription.php
@@ -11,63 +11,13 @@
   <title>Login Example - Semantic</title>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
 
   <script src="assets/library/jquery.min.js"></script>
   <script src="../dist/components/form.js"></script>
   <script src="../dist/components/transition.js"></script>
-
-  <style type="text/css">
-    body {
-      background-color: #DADADA;
-    }
-    body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
-    }
-    .column {
-      max-width: 450px;
-    }
-  </style>
-  <script>
-  $(document)
-    .ready(function() {
-      $('.ui.form')
-        .form({
-          fields: {
-            email: {
-              identifier  : 'email',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your e-mail'
-                },
-                {
-                  type   : 'email',
-                  prompt : 'Please enter a valid e-mail'
-                }
-              ]
-            },
-            password: {
-              identifier  : 'password',
-              rules: [
-                {
-                  type   : 'empty',
-                  prompt : 'Please enter your password'
-                },
-                {
-                  type   : 'length[6]',
-                  prompt : 'Your password must be at least 6 characters'
-                }
-              ]
-            }
-          }
-        })
-      ;
-    })
-  ;
-  </script>
+  <script src="js/script.js"></script>
 </head>
 <body>
   <div id="large-header" class="large-header">
@@ -148,85 +98,6 @@
      </div>
   
  </div>
- <style type="text/css">
-  #column{
-    position: absolute;
-    bottom: 20%;
-  }
- .large-header {
-   position: relative;
-   width: 100%;
-   background: #111;
-   overflow: hidden;
-   background-size: cover;
-   background-position: center center;
-   z-index: 1;
-  }
-
-  .demo .large-header {
-     background-image: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/demo-bg.jpg");
-  }
-
-  .main-title {
-     position: absolute;
-     margin: 0;
-     padding: 0;
-     color: #F9F1E9;
-     text-align: center;
-     top: 13%;
-     left: 50%;
-     -webkit-transform: translate3d(-50%, -50%, 0);
-     transform: translate3d(-50%, -50%, 0);
-  }
-
-  .demo .main-title {
-     text-transform: uppercase; 
-     letter-spacing: 0.1em;
-  }
-
-  .main-title .thin {
-     font-weight: 200;
-  }
-
-  ul{
-      max-width: 300px;
-    }
-    ul li{
-
-      color: #dadada;
-        background-color: rgb(118, 132, 131);
-        font-size: 22px;
-        margin-top: 22px;
-        padding: 7px 0px 7px 18px;
-        border-bottom-right-radius: 4px;
-        border-top-right-radius: 4px;
-        transition: 0.6s all;
-    }
-    ul li:hover{
-      transition: 0.6s all;
-      background-color:rgba(9, 0, 90, 0.4);
-    }
-    .sites{
-      position: absolute;
-      top: 24%;
-      /*left: 20%;*/
-      margin: 10px;
-    }
-
-  @media only screen and (max-width: 768px) {
-     .demo .main-title {
-        font-size: 3em;
-     }
-  }
-    </style>
-  <script>
-    window.console = window.console || function(t) {};
-  </script>
-  <script>
-    if (document.location.search.match(/type=embed/gi)) {
-      window.parent.postMessage("resize", "*");
-    }
-  </script>
   <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-de7e2ef6bfefd24b79a3f68b414b87b8db5b08439cac3f1012092b2290c719cd.js"></script>
   <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/TweenLite.min.js"></script>
   <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/EasePack.min.js"></script>
