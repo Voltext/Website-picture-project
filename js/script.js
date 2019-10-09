65efb215c9307debaac24ac8aaa652f67aafb1a0
soufiane <soufiane_dri130@hotmail.fr>
2019-10-09 13:48:42 +0200

    organisation du code

--- js/script.js
+++ js/script.js
@@ -33,4 +33,10 @@
         })
       ;
     })
-  ;
+  ;+
+
+  window.console = window.console || function(t) {};
+
+   if (document.location.search.match(/type=embed/gi)) {
+      window.parent.postMessage("resize", "*");
+    }
